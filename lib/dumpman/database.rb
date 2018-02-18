c8236e9c8ef6479d3505b194ed17512246f4c73a
rafael <skcc321@gmail.com>
2018-02-19 00:42:59 +0200

    fixed mysql strategy adaptor

--- lib/dumpman/database.rb
+++ lib/dumpman/database.rb
@@ -13,7 +13,7 @@ module Dumpman
     def strategy
       case ActiveRecord::Base.connection_config.fetch(:adapter)
       when 'postgresql' then Dumpman::Adapters::Pg
-      when 'mysql' then Dumpman::Adapters::Mysql
+      when 'mysql', 'mysql2' then Dumpman::Adapters::Mysql
       else
         raise('unknown adapter in "config/database.yml"')
       end
