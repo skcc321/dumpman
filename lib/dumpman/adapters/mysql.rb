01f3d9ed6d00825547139909979f68e56398fa33
rafael <skcc321@gmail.com>
2018-02-19 00:50:31 +0200

    no password access

--- lib/dumpman/adapters/mysql.rb
+++ lib/dumpman/adapters/mysql.rb
@@ -2,11 +2,19 @@ module Dumpman
   module Adapters
     class Mysql < Base
       def dump_cmd
-        "mysqldump -u #{username} -p #{password} -h #{host} -d #{database} > #{Dumpman.dump_file}"
+        if password.present?
+          "mysqldump -u #{username} -p #{password} -h #{host} -d #{database} > #{Dumpman.dump_file}"
+        else
+          "mysqldump -u #{username} -h #{host} -d #{database} > #{Dumpman.dump_file}"
+        end
       end
 
       def restore_cmd
-        "mysql -u #{username} -p #{password} -h #{host} #{database} < #{Dumpman.dump_file}"
+        if password.present?
+          "mysql -u #{username} -p #{password} -h #{host} #{database} < #{Dumpman.dump_file}"
+        else
+          "mysql -u #{username} -h #{host} #{database} < #{Dumpman.dump_file}"
+        end
       end
     end
   end
