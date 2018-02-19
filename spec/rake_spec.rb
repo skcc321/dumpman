b4b1bdcca1a750742ccad86b803bf0dab427a980
rafael <skcc321@gmail.com>
2018-02-20 01:34:25 +0200

    u

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -3,19 +3,27 @@ require "spec_helper"
 
 RSpec.describe Dumpman do
   context 'rake task' do
-    describe 'db:dump' do
-      it "creates db dump file" do
-        expect do
-          Rake::Task['db:dump'].invoke
-        end.to change { File.exists?(Dumpman.dump_file_name) }.from(false).to(true)
+    context 'conservation' do
+      describe 'db:dump' do
+        it "creates db dump file" do
+          expect do
+            Rake::Task['db:dump'].invoke
+          end.to change { File.exists?(Dumpman.dump_file_name) }.from(false).to(true)
+        end
+      end
+
+      describe 'db:compress' do
+        it "creates zip file with db dump" do
+          expect do
+            Rake::Task['db:compress'].invoke
+          end.to change { File.exists?(Dumpman.dump_zip_name) }.from(false).to(true)
+        end
       end
     end
 
-    describe 'db:compress' do
-      it "creates zip file with db dump" do
-        expect do
-          Rake::Task['db:compress'].invoke
-        end.to change { File.exists?(Dumpman.dump_zip_name) }.from(false).to(true)
+    context 'extraction' do
+      before do
+        PostgresqlUser.create(name: 'test name')
       end
     end
   end
