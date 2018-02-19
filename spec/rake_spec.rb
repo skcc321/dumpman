721a45e2038877c525b18c7ca4c1cf7edc4637fc
rafael <skcc321@gmail.com>
2018-02-20 01:31:07 +0200

    db dump

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -3,6 +3,14 @@ require "spec_helper"
 
 RSpec.describe Dumpman do
   context 'rake task' do
+    describe 'db:dump' do
+      it "creates db dump file" do
+        expect do
+          Rake::Task['db:dump'].invoke
+        end.to change { File.exists?(Dumpman.dump_file_name) }.from(false).to(true)
+      end
+    end
+
     describe 'db:compress' do
       it "creates zip file with db dump" do
         expect do
