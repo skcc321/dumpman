a523e2b52e9c011f91e4ce7ce5cf51f130ebdfbe
rafael <skcc321@gmail.com>
2018-02-20 15:54:10 +0200

    simplecov

--- spec/extraction_spec.rb
+++ spec/extraction_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Dumpman do
   context 'extraction' do
     describe 'db:restore' do
       it "restores db from dump file" do
+        PostgresUser.destroy_all
         PostgresUser.create(name: 'test name')
         Rake::Task['db:dump'].reenable
         Rake::Task['db:dump'].invoke
